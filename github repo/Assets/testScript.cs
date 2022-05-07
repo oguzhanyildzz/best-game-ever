31ced79dae24ca2406614703301c6a74cce78d6e
oguzhanyildzz <oguzhanyildiz900@gmail.com>
2022-05-07 19:03:42 +0300

    add script

---
+++ github repo/Assets/testScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //selam
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
