4faf313ddc2cbed15ad4ea77a20a98be489406be
oguzhanyildzz <oguzhanyildiz900@gmail.com>
2022-05-07 22:23:04 +0300

    updates

---
+++ github repo/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class playerMovement : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
