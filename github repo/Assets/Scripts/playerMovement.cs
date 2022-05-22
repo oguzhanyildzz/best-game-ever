399e9cd83c2a32367db3869d25df5aa2f232c9b0
oguzhanyildzz <oguzhanyildiz900@gmail.com>
2022-05-22 21:55:33 +0300

    genel değişiklikşler

--- github repo/Assets/Scripts/playerMovement.cs
+++ github repo/Assets/Scripts/playerMovement.cs
@@ -6,17 +6,52 @@ using UnityEngine.InputSystem;
 
 public class playerMovement : MonoBehaviour
 {
-    
+    [SerializeField] float runSpeed = 10f;
+
+    Vector2 moveInput;//karakterin hareketi hakkýnda alýk bilgi
+    Rigidbody2D rb2d;
+    Animator myAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Run();
+        FlipSprite();//karakter hareket ederken bakýþ yönünü ayarlama
     }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, rb2d.velocity.y);// aþaðý yukarý gitmeyi engelliyo
+        rb2d.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(rb2d.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isWalking", playerHasHorizontalSpeed);
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(rb2d.velocity.x) > Mathf.Epsilon;
+
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rb2d.velocity.x), 1f);
+        }
+    }
+
+
+
+
+
 }
