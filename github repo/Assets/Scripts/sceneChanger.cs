824b17682568c909ce34d3eb0424d9f26ffa3834
oguzhanyildzz <oguzhanyildiz900@gmail.com>
2022-06-06 13:30:07 +0300

    zoert ayar

---
+++ github repo/Assets/Scripts/sceneChanger.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void NextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
